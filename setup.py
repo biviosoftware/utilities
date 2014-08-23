63dc2d9ffba3a136d94294dc17f5f62782b8f500
robnagler <github@q33.us>
2014-08-23 15:09:14 -0600

    backup and restore working

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='utilities',
+    version='1.0',
+    description='various tools',
+    author='Rob Nagler',
+    author_email='nagler@bivio.biz',
+    scripts=['bin/github'])
