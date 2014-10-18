4efd7e2f41c6e38c59aba4e7a4702aa9b7e653a2
robnagler <github@q33.us>
2014-10-18 18:06:04 +0000

    fpc

--- bin/fixup-docker.sh
+++ bin/fixup-docker.sh
@@ -4,12 +4,11 @@ cp -a /etc/skel/.??* /root
 cat > /.bashrc << 'EOF'
 export HOME=/root
 cd $HOME
-. ~/.bash_profile
+. /root/.bash_profile
 EOF
 cat >> /root/.bashrc << 'EOF'
 export HOSTNAME=docker
 export PROMPT_COMMAND=
 export PS1='[docker \W]\$ '
 EOF
-cd /
-. .bashrc
+. /.bashrc
